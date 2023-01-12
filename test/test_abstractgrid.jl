01e64df4cc6c668f2c2b0fa81f6e12484cfd29a0
Thomas Burton <tburton5572@gmail.com>
2023-01-12 12:02:41 +0000

    added skeleton test suite and comments about how to do the unit testing

--- test/test_abstractgrid.jl
+++ test/test_abstractgrid.jl
@@ -1,14 +1,6 @@
 # This file contains the tests for a basic concrete implementation of
 # AbstractGrid.
 
-# TODO: add the required methods
-
-struct Grid <: AbstractGrid end
-
-@testset "Grid points                           " begin
-
-end
-
 @testset "Grid size                             " begin
 
 end
