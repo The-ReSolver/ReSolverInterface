de3320f142ff50863f58334dbe103544a0726398
Thomas Burton <tburton5572@gmail.com>
2023-11-30 09:48:35 +0000

    RPCF grid concrete type tested

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,13 @@
 using ReSolverInterface
 using Test
 using LinearAlgebra
+using FFTW
 
 using FDGrids
 
-include("test_fakes.jl")
+include("rpcf_test.jl")
 
 include("test_notimplementederror.jl")
 include("test_abstractgrid.jl")
-include("test_abstractscalarfield.jl")
-include("test_vectorfield.jl")
+# include("test_abstractscalarfield.jl")
+# include("test_vectorfield.jl")
--- test/test_abstractgrid.jl
+++ test/test_abstractgrid.jl
@@ -1,8 +1,30 @@
-@testset "Abstract grid                         " begin
-    # construct my grid
-    grid = MyGrid([(-1 + 0.2*i, 2π/4*j) for i in 0:3, j in 0:3])
+@testset "Test RPCF Example                     " begin
+    # generate random inputs
+    Ny = rand(3:20)
+    Nz = rand(3:20)
+    Nt = rand(3:20)
+    randD = rand([-2, -1, 1, 2])
+    y = rand(Float64, Ny)
+    D1 = rand(Float32, (Ny, Ny))
+    D2 = rand(Float16, (Ny + randD, Ny + randD))
+    D_sec = rand(Float32, (Ny, Ny))
+    w1 = rand(Int128, Ny)
+    w2 = rand(Float16, Ny + randD)
+    ω = abs(randn())
+    β = abs(rand())
 
-    @test points(grid) isa Matrix{Tuple{Float64, Float64}}
-    @test grid == MyGrid(points(grid))
-    @test !(grid == MyGrid(points(grid)[1:end-1, :]))
+    # test point generation
+    g1 = RPCFGrid(y, Nz, Nt, D1, D_sec, w1, ω, β)
+    pts = points(g1)
+    z = range(0, 2π*(1 - 1/Nz), length = Nz)/β # precision differences in operations
+    t = range(0, 2π*(1 - 1/Nt), length = Nt)/ω # mean they aren't exactly equal
+    @test collect.(pts) ≈ [[y[ny], z[nz], t[nt]] for ny in 1:Ny, nz in 1:Nz, nt in 1:Nt]
+
+    # test comparison
+    g2 = RPCFGrid(y, Nz, Nt + 1, D1, D_sec, w1, ω, β)
+    g3 = RPCFGrid(rand(Float64, Ny), Nz, Nt, D1, D_sec, w1, ω, β)
+    g4 = RPCFGrid(y, Nz, Nt, rand(Float64, (Ny, Ny)), D_sec, w1, ω, β)
+    @test g1 != g2
+    @test g1 != g3
+    @test g1 == g4
 end
