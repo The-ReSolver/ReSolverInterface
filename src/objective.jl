3068ec96ad3489e2396de544fc6d64c815bfe07d
Thomas Burton <tburton5572@gmail.com>
2024-04-01 11:23:11 +0100

    rework of objective cache for new projected scalar field

--- src/objective.jl
+++ src/objective.jl
@@ -7,10 +7,10 @@
 # be kept generic as they rely on the vector calculus operations. To help with
 # this a set of methods to extract the cache variables can be created.
 
-struct Objective{T, S<:AbstractScalarField, SP<:AbstractScalarField, B<:AbstractArray, M<:AbstractArray}
+struct Objective{T, N, S<:AbstractScalarField, B<:AbstractArray, M<:AbstractArray}
     grad::VectorField{3, S}
     cache::Vector{S}
-    projectedCache::Vector{SP}
+    projectedCache::Vector{ProjectedScalarField{N, S, T, M}}
     base::B
     modes::M
     params::Vector{T}
@@ -21,16 +21,24 @@ struct Objective{T, S<:AbstractScalarField, SP<:AbstractScalarField, B<:Abstract
         grad = similar(field)
 
         # initialise cache
-        cache = [S(g) for _ in 1:1]
-        projectedCache = [S(g, modes) for _ in 1:1]
+        cache = [similar(field) for _ in 1:1]
+        projectedCache = [ProjectedScalarField(modes, similar(field)) for _ in 1:1]
+
+        params = convert.(eltype(field), params)
 
         new{T, S, typeof(projectedCache), B, M}(grad, cache, projectedCache, base, modes, params, free_mean)
     end
 end
 
 function (f::Objective{<:Any, <:Any, SP})(a::SP) where {SP}
-    
+
 end
+function (f::ResGrad)(F, G, a::SpectralField)
+    G === nothing ? F = f(a, false)[2] : (F = f(a, true)[2]; G .= f.out)
+    return F
+end
+# TODO: conversion of field to real vector
+(f::ResGrad)(x::Vector{T}) where {T<:AbstractFloat} = f(_vectorToVelocityCoefficients!(f.proj_cache[2], x), false)[2]
 
 function _update_velocity!(cache) end
 
