1b2992a5d64af486c44b923f0f5152ade2687069
Thomas Burton <tburton5572@gmail.com>
2024-04-02 15:41:13 +0100

    basic objective implementation

--- src/objective.jl
+++ src/objective.jl
@@ -2,27 +2,28 @@
 # the optimisation variables. In addition it will work as a functor to allow
 # it to directly be used to compute the residuals.
 
-# To get a general version of this working I have to allow the user to determine 
-# what their Navier-Stokes equation looks like. The cache update functions can
-# be kept generic as they rely on the vector calculus operations. To help with
-# this a set of methods to extract the cache variables can be created.
+# To get a general version of this working I have to allow the user to
+# determine what their Navier-Stokes equation looks like. The cache update
+# functions can be kept generic as they rely on the vector calculus operations.
+# To help with this a set of methods to extract the cache variables can be
+# created.
 
-struct Objective{T, N, S<:AbstractScalarField, B<:AbstractArray, M<:AbstractArray}
+struct Objective{S, T, N, B<:AbstractArray, M<:AbstractArray}
     grad::VectorField{3, S}
-    cache::Vector{S}
+    cache::Vector{VectorField{3, S}}
     projectedCache::Vector{ProjectedScalarField{N, S, T, M}}
     base::B
     modes::M
     params::Vector{T}
     free_mean::Bool
 
-    function Objective(field::S, modes::M, base::B, params::Vector{T}, free_mean::Bool) where {S<:AbstractScalarField, M, B, T}
+    function Objective(::Type{S}, grid::AbstractGrid, modes::M, base::B, params::Vector{T}, free_mean::Bool) where {S<:AbstractScalarField, M, B, T}
         # initialise residual gradient output
-        grad = similar(field)
+        grad = S(g)
 
         # initialise cache
-        cache = [similar(field) for _ in 1:1]
-        projectedCache = [ProjectedScalarField(modes, similar(field)) for _ in 1:1]
+        cache = [VectorField(S, g) for _ in 1:4]
+        projectedCache = [ProjectedScalarField(modes, S(g)) for _ in 1:3]
 
         params = convert.(eltype(field), params)
 
@@ -30,19 +31,40 @@ struct Objective{T, N, S<:AbstractScalarField, B<:AbstractArray, M<:AbstractArra
     end
 end
 
-function (f::Objective{<:Any, <:Any, SP})(a::SP) where {SP}
+function (f::Objective{S})(a::ProjectedScalarField{S}, compute_grad::Bool=true) where {S}
+    # assign aliases
+    u    = f.cache[1]
+    dudt = f.cache[2]
+    r    = f.cache[3]
+    drdt = f.cache[4]
+    s    = f.projectedCache[1]
+    N_a  = f.projectedCache[2]
+    M_as = f.projectedCache[3]
 
+    # expand velocity coefficients into velocity field
+    expand!(u, a)
+
+    # include the base profile
+    include_base!(u, f.base)
+
+    # compute local residual
+    @. r = ddt!(dudt, u) - navierStokesRHS!(N_a, a)
+    expand!(r, project!(s, r))
+
+    # compute gradient
+    if compute_grad
+        @. dRdu = ddt!(drdt, r) + gradientRHS!(M_as, a, s)
+        project!(f.grad, dRdu)
+    end
+
+    return f.grad, norm(s)^2/volume(grid(u))
 end
-function (f::ResGrad)(F, G, a::SpectralField)
-    G === nothing ? F = f(a, false)[2] : (F = f(a, true)[2]; G .= f.out)
+function (f::Objective{S})(F, G, a::ProjectedScalarField{S}) where {S}
+    G === nothing ? F = f(a, false)[2] : (F = f(a, true)[2]; G .= f.grad)
     return F
 end
 # TODO: conversion of field to real vector
-(f::ResGrad)(x::Vector{T}) where {T<:AbstractFloat} = f(_vectorToVelocityCoefficients!(f.proj_cache[2], x), false)[2]
+(f::Objective)(x::Vector) = f(vectorToVelocityCoefficients!(f.proj_cache[2], x), false)[2]
 
-function _update_velocity!(cache) end
-
-function _update_residual!(cache) end
-
-ns(::Objective) = throw(NotImplementedError())
-grad(::Objective) = throw(NotImplementedError())
+navierStokesRHS!(N_a, a) = throw(NotImplementedError(N_a, a))
+gradientRHS!(M_as, a, s) = throw(NotImplementedError(M_as, a, s))
