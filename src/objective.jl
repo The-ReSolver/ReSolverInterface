c581ecc7d154c31d1c752e2e3cbf2090900f73d3
Thomas Burton <tburton5572@gmail.com>
2024-04-06 17:55:09 +0100

    default operator arguments to compute residual and gradient

--- src/objective.jl
+++ src/objective.jl
@@ -1,28 +1,20 @@
-# This file contains the definition of the cache object that keeps track of all
-# the optimisation variables. In addition it will work as a functor to allow
-# it to directly be used to compute the residuals.
+# Definition of the cache object that keeps track of all the optimisation
+# variables. In addition it will work as a functor to allow it to directly be
+# used to compute the residuals.
 
-# To get a general version of this working I have to allow the user to
-# determine what their Navier-Stokes equation looks like. The cache update
-# functions can be kept generic as they rely on the vector calculus operations.
-# To help with this a set of methods to extract the cache variables can be
-# created.
-
-# FIXME: type parameter doesn't allow for abstract vector field, only my concrete implementation
-struct Objective{S, T, N, B<:AbstractArray, M<:AbstractArray, NSE, VDE}
-    grad::ProjectedScalarField{N, S, T, M}
-    cache::Vector{VectorField{3, S}}
-    projectedCache::ProjectedScalarField{N, S, T, M}
+struct Objective{S, T, D, N, B<:AbstractArray, M<:AbstractArray, NSE, VDE}
+    grad::ProjectedScalarField{D, S, T, M}
+    cache::Vector{VectorField{N, S}}
+    projectedCache::ProjectedScalarField{S, D, T, M}
     base::B
     modes::M
     free_mean::Bool
-    navierStokesOperator!::NSE
-    gradientOperator!::VDE
+    navierStokesOperator::NSE
+    gradientOperator::VDE
 
-    # TODO: if nothing is provided for operators then provide default
-    function Objective(::Type{S}, grid::AbstractGrid, modes::M, base::B, navierStokesOperator!, gradientOperator!, free_mean::Bool) where {S<:AbstractScalarField, M, B, T}
+    function Objective(::Type{S}, grid::AbstractGrid, Re::Float64, modes::M, base::B, free_mean::Bool, navierStokesOperator=NavierStokesOperator(S, grid, Re), gradientOperator=GradientOperator(S, grid, Re)) where {S<:AbstractScalarField, M, B, T}
         # initialise residual gradient output
-        grad = S(g)
+        grad = ProjectedScalarField(modes, S(g))
 
         # initialise cache
         cache = [VectorField(S, g) for _ in 1:6]
@@ -30,7 +22,7 @@ struct Objective{S, T, N, B<:AbstractArray, M<:AbstractArray, NSE, VDE}
 
         params = convert.(eltype(field), params)
 
-        new{S, eltype(grad), ndims(grad), B, M, typeof(navierStokesOperator!), typeof(gradientOperator!)}(grad, cache, projectedCache, base, modes, params, free_mean, navierStokesOperator!, gradientOperator!)
+        new{S, eltype(grad), ndims(grad), length(cache[1]), B, M, typeof(navierStokesOperator!), typeof(gradientOperator!)}(grad, cache, projectedCache, base, modes, params, free_mean, navierStokesOperator, gradientOperator)
     end
 end
 
@@ -51,17 +43,18 @@ function (f::Objective{S})(a::ProjectedScalarField{S}, compute_grad::Bool=true) 
     include_base!(u, f.base)
 
     # compute local residual
-    @. r = ddt!(dudt, u) - f.navierStokesOperator!(N_u, u)
+    @. r = ddt!(dudt, u) - f.navierStokesOperator(N_u, u)
     expand!(r, project!(s, r))
 
     # compute gradient
     if compute_grad
-        @. dRdu = ddt!(drdt, r) + f.gradientOperator!(M_ur, u, r)
+        @. dRdu = ddt!(drdt, r) + f.gradientOperator(M_ur, u, r)
         project!(f.grad, dRdu)
     end
 
     return f.grad, norm(s)^2/volume(grid(u))
 end
+
 function (f::Objective{S})(F, G, a::ProjectedScalarField{S}) where {S}
     G === nothing ? F = f(a, false)[2] : (F = f(a, true)[2]; G .= f.grad)
     return F
